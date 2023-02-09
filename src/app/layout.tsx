ac7af1177bb8be85a58a70fc5a991825be14a1f3
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-02-09 23:26:44 +0530

    Add providers where needed
    Only add ContextProviders to layouts where they're needed.
    i.e., If `/blogs` path requires the auth provider then add that
    auth provider in the layout file of the blog route (in `/app/blogs/layouts.tsx`)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import { ChildrenProps } from '@/utils/types';
 import '../styles/globals.css';
-import Provider from './providers';
 
 export default function RootLayout({ children }: ChildrenProps) {
   return (
     // TODO: Use `next-themes` to toggle dark mode
     <html lang="en" className="dark">
       <head />
-      <body>
-        <Provider>{children}</Provider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
