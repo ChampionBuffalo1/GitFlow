922783bbd0091ec2fa7ede4ec224789c1e352a1d
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-02-11 22:29:11 +0530

    Added somewhat functioning navbar

---
+++ src/app/navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    // bg-blend-darken
+    <header className="fixed flex justify-between w-full px-3 py-2 text-white bg-gray-600 ">
+      <span className="text-2xl">
+        <Link href="/">GitFlow</Link>
+      </span>
+      <span className="flex w-1/6 justify-evenly">
+        <Link href="/about">About</Link>
+        <Link href="https://github.com/ChampionBuffalo1/Gitwork" prefetch={false}>
+          Github
+        </Link>
+      </span>
+    </header>
+  );
+}
