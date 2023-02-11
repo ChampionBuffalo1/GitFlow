688052626eaf8a9e1175b3c99dc1ed09b100522d
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-02-11 22:27:54 +0530

    Using roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
-import { ChildrenProps } from '@/utils/types';
 import '../styles/globals.css';
+import NavBar from './navbar';
+import { Roboto } from '@next/font/google';
+import { ChildrenProps } from '@/utils/types';
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin']
+});
 
 export default function RootLayout({ children }: ChildrenProps) {
   return (
     // TODO: Use `next-themes` to toggle dark mode
-    <html lang="en" className="dark">
+    <html lang="en" className={`${roboto.className} dark`}>
       <head />
-      <body>{children}</body>
+      <body className="bg-gradient-to-r from-slate-900 to-slate-700">
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
