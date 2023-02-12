849cf74a4ef213000f1df67d9d198a6bca63a6ef
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-02-12 16:50:40 +0530

    Fixed styles

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -3,16 +3,18 @@ import Link from 'next/link';
 export default function NavBar() {
   return (
     // bg-blend-darken -> bg-grey-600 ??
-    <header className="fixed flex justify-between w-full px-3 py-2 text-white bg-gray-600 ">
-      <span className="text-2xl">
-        <Link href="/">GitFlow</Link>
-      </span>
-      <span className="flex w-1/6 justify-evenly">
-        <Link href="/about">About</Link>
-        <Link href="https://github.com/ChampionBuffalo1/Gitwork" prefetch={false}>
-          Github
-        </Link>
-      </span>
+    <header>
+      <nav className="fixed flex justify-between w-full px-3 py-2 text-white bg-gray-600">
+        <span className="text-2xl">
+          <Link href="/">GitFlow</Link>
+        </span>
+        <span className="px-2 space-x-4 text-xl lg:space-x-12 lg:px-12">
+          <Link href="/about">About</Link>
+          <Link href="https://github.com/ChampionBuffalo1/Gitwork" prefetch={false} target={'_blank'}>
+            Github
+          </Link>
+        </span>
+      </nav>
     </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div id="full-window" className="flex items-center justify-center text-gray-100">
-      <form action="/networks/github/" method="GET" className="w-[25rem]">
+    <div id="full-window" className="flex items-center justify-center overflow-hidden text-gray-100">
+      <form action="/networks/github/" method="GET" className="w-[20rem]">
         <Image
           src="/gh-white.png"
-          width={80}
-          height={80}
+          width={95}
+          height={95}
           alt="Github Logo"
-          className="relative float-left w-auto h-auto mx-4"
+          className="relative float-left w-auto h-auto mx-4 mb-5"
         />
         <label htmlFor="username" className="block text-2xl font-medium m-7">
           Enter your username
@@ -17,7 +17,7 @@ export default function Home() {
         <br />
         <input
           type="text"
-          className="p-5 m-3 rounded-full bg-slate-600 w-[inherit]"
+          className="w-full p-5 m-3 rounded-full bg-slate-600"
           name="username"
           placeholder="Github username"
           required
