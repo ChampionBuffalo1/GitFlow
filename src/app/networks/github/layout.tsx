dd49ea86800a261c2baee6d720bff1b103567307
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-02-15 10:12:53 +0530

    Removing redundant mt-12

--- src/app/networks/github/layout.tsx
+++ src/app/networks/github/layout.tsx
@@ -3,7 +3,7 @@ import SwrProvider from './swrProvider';
 
 export default function Layout({ children }: ChildrenProps) {
   return (
-    <div className="w-full h-screen mt-12 overflow-hidden">
+    <div className="w-full h-screen overflow-hidden">
       <SwrProvider>{children}</SwrProvider>;
     </div>
   );
